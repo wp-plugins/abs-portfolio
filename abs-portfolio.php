77f59eab5154eeca39b8a0598a008a658d5f7e34
siddiknmh <siddiknmh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-16 11:39:25 +0000

    add some functions
    git-svn-id: https://plugins.svn.wordpress.org/abs-portfolio/trunk@1091271 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- abs-portfolio.php
+++ abs-portfolio.php
@@ -85,7 +85,7 @@ function abs_portfolio_custompost() {
 		'has_archive'        => true,
 		'hierarchical'       => false,
 		'menu_position'      => null,
-		'supports'           => array( 'title','custom-fields','thumbnail' )
+		'supports'           => array( 'title','thumbnail' )
 	);
 
 	register_post_type( 'abs-portfolio-items', $args );
@@ -120,7 +120,6 @@ add_action( 'init', 'abs_portfolio_custom_post_taxonomy');
 function abs_portfolio_shortcode($atts){
 	extract( shortcode_atts( array(
 		'category' => '',
-		'link' => 'www.absiddik.net',
 	), $atts, 'category_post' ) );
 	
     $q = new WP_Query(
@@ -131,11 +130,16 @@ function abs_portfolio_shortcode($atts){
 
 	while($q->have_posts()) : $q->the_post();
 		//get the ID of your post in the loop
-		$idd = get_the_ID();
+		$id = get_the_ID();
 		
 		$tn_id = get_post_thumbnail_id();
 		$abs_por_image = wp_get_attachment_image_src( $tn_id, 'abs_por_image');
-		$portfolio_link = get_post_meta($idd, 'portfolio_link', true);
+		
+		
+		global $post;
+		$portfolio_link = get_post_meta( $post->ID, 'portfolio_link', true );
+		
+	
 		
 		$list .= '
 				<li>
@@ -175,14 +179,102 @@ function abs_portfolio_awesome_buttons($buttons) {
 add_action ('init', 'abs_portfolio_buttons');
 
 
+/**
+ * Custom mata box for ABS Portfolio
+ */
+function abs_portfolio_add_meta_box() {
 
+	$screens = array( 'abs-portfolio-items' );
 
+	foreach ( $screens as $screen ) {
 
+		add_meta_box(
+			'abs_portfolio_meta_box_id',
+			__( 'Portfolio Link', 'abs_portfolio_plugin_textdomain' ),
+			'abs_portfolio_plugin_meta_box_callback',
+			$screen
+		);
+	}
+}
+add_action( 'add_meta_boxes', 'abs_portfolio_add_meta_box' );
 
+/**
+ * Prints the box content.
+ * 
+ * @param WP_Post $post The object for the current post/page.
+ */
+function abs_portfolio_plugin_meta_box_callback( $post ) {
 
+	// Add an nonce field so we can check for it later.
+	wp_nonce_field( 'abs_portfolio_plugin_meta_box', 'abs_portfolio_plugin_meta_box_nonce' );
 
+	/*
+	 * Use get_post_meta() to retrieve an existing value
+	 * from the database and use the value for the form.
+	 */
+	$portfolio_link = get_post_meta( $post->ID, 'portfolio_link', true );
 
+	echo '<label for="abs_portfolio_new_field">';
+	_e( 'Put link without http:// and https://', 'abs_portfolio_plugin_textdomain' );
+	echo '</label> ';
+	echo '<input type="text" id="abs_portfolio_new_field" name="abs_portfolio_new_field" value="' . esc_attr( $portfolio_link ) . '" size="25" />';
+}
 
+/**
+ * When the post is saved, saves our custom data.
+ *
+ * @param int $post_id The ID of the post being saved.
+ */
+function abs_portfolio_save_meta_box_data( $post_id ) {
+
+	/*
+	 * We need to verify this came from our screen and with proper authorization,
+	 * because the save_post action can be triggered at other times.
+	 */
+
+	// Check if our nonce is set.
+	if ( ! isset( $_POST['abs_portfolio_plugin_meta_box_nonce'] ) ) {
+		return;
+	}
+
+	// Verify that the nonce is valid.
+	if ( ! wp_verify_nonce( $_POST['abs_portfolio_plugin_meta_box_nonce'], 'abs_portfolio_plugin_meta_box' ) ) {
+		return;
+	}
+
+	// If this is an autosave, our form has not been submitted, so we don't want to do anything.
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
+
+	// Check the user's permissions.
+	if ( isset( $_POST['post_type'] ) && 'page' == $_POST['post_type'] ) {
+
+		if ( ! current_user_can( 'edit_page', $post_id ) ) {
+			return;
+		}
+
+	} else {
+
+		if ( ! current_user_can( 'edit_post', $post_id ) ) {
+			return;
+		}
+	}
+
+	/* OK, it's safe for us to save the data now. */
+	
+	// Make sure that it is set.
+	if ( ! isset( $_POST['abs_portfolio_new_field'] ) ) {
+		return;
+	}
+
+	// Sanitize user input.
+	$my_data = sanitize_text_field( $_POST['abs_portfolio_new_field'] );
+
+	// Update the meta field in the database.
+	update_post_meta( $post_id, 'portfolio_link', $my_data );
+}
+add_action( 'save_post', 'abs_portfolio_save_meta_box_data' );
 
 
 
